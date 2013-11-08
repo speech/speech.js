ec410001d1fed33e122443596dae9f135177dc20
indolering <zachlym@indolering.com>
2013-11-08 14:11:15 -0800

    pheonix

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('[speechjs]App', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
